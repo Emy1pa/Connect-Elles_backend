3bb14a84ef84888c0f63283c739ea61a69f4b320
imane souaouti <souaouti.iman@gmail.com>
2025-01-26 12:25:50 +0100

    CNL-10 CNL-11: feat(profile): Add endpoints for user profile and admin account management
    - Implemented API for current user to fetch their own profile
    - Added API for admin to retrieve all user accounts with role-based access control
    - Secured endpoints with JWT validation and role checking
    - Enhanced error handling and response structure for both endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { RegisterDto } from './dtos/register.dto';
+import { LoginDto } from './dtos/login.dto';
+import { JWTPayloadType } from 'src/utils/types';
+import { AuthGuard } from './guards/auth.guard';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { Roles } from './decorators/user-role.decorator';
+import { UserRole } from 'src/utils/enums';
+import { AuthRolesGuard } from './guards/auth-roles.guard';
 
-@Controller()
-export class UsersController {}
+@Controller('api/users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+  @Post('auth/register')
+  public register(@Body() registerDto: RegisterDto) {
+    return this.usersService.register(registerDto);
+  }
+  @Post('auth/login')
+  @HttpCode(HttpStatus.OK)
+  public login(@Body() body: LoginDto) {
+    return this.usersService.login(body);
+  }
+  @Get('current-user')
+  @UseGuards(AuthGuard)
+  public getCurrentUser(@CurrentUser() payload: JWTPayloadType) {
+    return this.usersService.getCurrentUser(payload.id);
+  }
+  @Get()
+  @Roles(UserRole.ADMIN)
+  @UseGuards(AuthRolesGuard)
+  public getAllUsers() {
+    return this.usersService.getAll();
+  }
+}
