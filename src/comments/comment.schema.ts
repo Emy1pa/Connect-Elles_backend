af5bf2df38768ec9f150fdf99593d961ca2980e1
imane souaouti <souaouti.iman@gmail.com>
2025-02-24 17:14:13 +0000

    CNL-53: Create comments schema with userId, postId, content, createdAt, and updatedAt fields

---
+++ src/comments/comment.schema.ts
@@ -0,0 +1,13 @@
+import { Document, Types } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+export type FavoriteDocument = Comment & Document;
+@Schema({ timestamps: true })
+export class Comment {
+  @Prop({ required: true, minlength: 1 })
+  text: string;
+  @Prop({ type: Types.ObjectId, ref: 'Blog', required: true })
+  blog: Types.ObjectId;
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+  user: Types.ObjectId;
+}
+export const CommentSchema = SchemaFactory.createForClass(Comment);
