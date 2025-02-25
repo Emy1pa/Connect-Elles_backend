7325cc532d6967a214fab873537fd2a8b896ae6c
imane souaouti <souaouti.iman@gmail.com>
2025-02-25 09:58:19 +0000

    CNL-55: Implement fetching all comments and retrieving comments by user

--- src/comments/comment.service.ts
+++ src/comments/comment.service.ts
@@ -1,10 +1,15 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
 import { CommentDocument } from './comment.schema';
 import { User, UserDocument } from 'src/users/user.schema';
 import { Blog, BlogDocument } from 'src/blogs/blog.schema';
 import { CreateCommentDto } from './dtos/create-comment.dto';
+import { text } from 'stream/consumers';
 
 @Injectable()
 export class CommentService {
@@ -42,7 +47,58 @@ export class CommentService {
         blog: newComment.blog._id.toString(),
       };
     } catch (error) {
-      throw new Error('Failed to create comment');
+      throw new BadRequestException(
+        `Failed to create comment: ${error.message}`,
+      );
+    }
+  }
+
+  async getAllComments() {
+    try {
+      const comments = await this.commentModel.find().lean().exec();
+      return comments.map((comment) => ({
+        ...comment,
+        _id: comment._id.toString(),
+        text: comment.text,
+        user: {
+          _id: comment.user._id.toString(),
+          blog: {
+            _id: comment.blog._id.toString(),
+          },
+        },
+      }));
+    } catch (error) {
+      throw new Error('Failed to retrieve comments');
+    }
+  }
+
+  async getUserComments(userId: string) {
+    try {
+      const existingUser = await this.userModel.findById(userId);
+      if (!existingUser) {
+        throw new NotFoundException('User not found');
+      }
+      const comments = await this.commentModel
+        .find({
+          user: new Types.ObjectId(userId),
+        })
+        .populate('blog', '_id title')
+        .lean()
+        .exec();
+      return comments.map((comment) => ({
+        _id: comment._id.toString(),
+        text: comment.text,
+        blog: comment.blog
+          ? {
+              _id: (comment.blog as any)._id.toString(),
+              title: (comment.blog as any).title,
+            }
+          : null,
+      }));
+    } catch (error) {
+      throw new BadRequestException(
+        `Failed to retrieve comments: ${error.message}`,
+      );
     }
   }
 }
