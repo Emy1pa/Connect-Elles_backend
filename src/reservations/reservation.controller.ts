8b26da5934cd3095b2f8dd6b6c4f0edad40bb6c6
imane souaouti <souaouti.iman@gmail.com>
2025-02-26 15:48:05 +0000

    CNL-74: feat: implement ReservationController with CRUD endpoints Added endpoints for creating, retrieving, and updating reservations, including status management.

---
+++ src/reservations/reservation.controller.ts
@@ -0,0 +1,73 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { ReservationService } from './reservation.service';
+import { AuthRolesGuard } from 'src/users/guards/auth-roles.guard';
+import { ReservationStatus, UserRole } from 'src/utils/enums';
+import { Roles } from 'src/users/decorators/user-role.decorator';
+import { CreateReservationDto } from './dtos/create-reservation.dto';
+import { UpdateReservationDto } from './dtos/update-reservation.dto';
+
+@Controller('reservations')
+export class ReservationController {
+  constructor(private reservationService: ReservationService) {}
+
+  @Post(':userId/:serviceId')
+  @UseGuards(AuthRolesGuard)
+  @Roles(UserRole.NORMAL_USER)
+  async createReservation(
+    @Param('serviceId') serviceId: string,
+    @Param('userId') userId: string,
+    @Body() createReservation: CreateReservationDto,
+  ) {
+    return this.reservationService.createReservation(
+      createReservation,
+      userId,
+      serviceId,
+    );
+  }
+  @Get()
+  async getAllReservations() {
+    return this.reservationService.getAllReservations();
+  }
+
+  @Get('user/:userId')
+  @UseGuards(AuthRolesGuard)
+  @Roles(UserRole.NORMAL_USER)
+  async getUserReservations(@Param('userId') userId: string) {
+    return this.reservationService.getUserReservations(userId);
+  }
+
+  @Get(':id')
+  @UseGuards(AuthRolesGuard)
+  @Roles(UserRole.NORMAL_USER)
+  async getReservationById(@Param('id') id: string) {
+    return this.reservationService.getReservationById(id);
+  }
+
+  @Patch(':id/status/:userId/:userRole')
+  @UseGuards(AuthRolesGuard)
+  @Roles(UserRole.NORMAL_USER)
+  async updateReservation(
+    @Param('id') reservationId: string,
+    @Param('userId') userId: string,
+    @Body() updateReservation: UpdateReservationDto,
+    @Param('userRole') userRole: UserRole,
+  ) {
+    const isMentor = userRole === UserRole.MENTOR;
+    const newStatus = (updateReservation as any).status as ReservationStatus;
+
+    return this.reservationService.updateReservationStatus(
+      reservationId,
+      userId,
+      newStatus,
+      isMentor,
+    );
+  }
+}
