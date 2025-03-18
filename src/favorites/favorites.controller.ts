b54500d4d94e8196b556916abbe35b30bba48f4d
imane souaouti <souaouti.iman@gmail.com>
2025-03-18 21:25:19 +0000

    CNL-136: Integrate Swagger documentation for Favorites controller. - Implemented Swagger API documentation for the Favorites routes. - Added detailed descriptions, request parameters, and response types.

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -11,13 +11,25 @@ import { FavoriteService } from './favorites.service';
 import { AuthRolesGuard } from 'src/users/guards/auth-roles.guard';
 import { Roles } from 'src/users/decorators/user-role.decorator';
 import { UserRole } from 'src/utils/enums';
-
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiParam,
+  ApiBody,
+} from '@nestjs/swagger';
+@ApiTags('Favorites')
 @Controller('favorites')
 export class FavoriteController {
   constructor(private readonly favoriteService: FavoriteService) {}
   @UseGuards(AuthRolesGuard)
   @Roles(UserRole.NORMAL_USER)
   @Post(':userId/:blogId')
+  @ApiOperation({ summary: 'Ajouter un favori pour un utilisateur' })
+  @ApiParam({ name: 'userId', type: String })
+  @ApiParam({ name: 'blogId', type: String })
+  @ApiResponse({ status: 201, description: 'Favori ajouté avec succès' })
+  @ApiResponse({ status: 400, description: 'Requête invalide' })
   async addFavorite(
     @Param('userId') userId: string,
     @Param('blogId') blogId: string,
@@ -32,6 +44,10 @@ export class FavoriteController {
   @UseGuards(AuthRolesGuard)
   @Roles(UserRole.NORMAL_USER)
   @Delete(':favoriteId')
+  @ApiOperation({ summary: 'Supprimer un favori' })
+  @ApiParam({ name: 'favoriteId', type: String })
+  @ApiResponse({ status: 200, description: 'Favori supprimé avec succès' })
+  @ApiResponse({ status: 400, description: 'Requête invalide' })
   async removeFavorite(@Param('favoriteId') favoriteId: string) {
     try {
       return await this.favoriteService.removeFavorite(favoriteId);
@@ -43,6 +59,12 @@ export class FavoriteController {
   @UseGuards(AuthRolesGuard)
   @Roles(UserRole.NORMAL_USER)
   @Get(':userId')
+  @ApiOperation({ summary: 'Récupérer les favoris d’un utilisateur' })
+  @ApiParam({ name: 'userId', type: String })
+  @ApiResponse({
+    status: 200,
+    description: 'Liste des favoris de l’utilisateur',
+  })
   async getUserFavorites(@Param('userId') userId: string) {
     try {
       return await this.favoriteService.getUserFavorite(userId);
@@ -52,6 +74,12 @@ export class FavoriteController {
   }
 
   @Get('statistics/:userId')
+  @ApiOperation({ summary: 'Obtenir le nombre de favoris d’un utilisateur' })
+  @ApiParam({ name: 'userId', type: String })
+  @ApiResponse({
+    status: 200,
+    description: 'Statistiques des favoris utilisateur',
+  })
   @UseGuards(AuthRolesGuard)
   @Roles(UserRole.MENTOR, UserRole.NORMAL_USER)
   async getFavorisStatistics(@Param('userId') userId: string) {
@@ -60,17 +88,28 @@ export class FavoriteController {
   @Get('statistics/mentor/:mentorId')
   @UseGuards(AuthRolesGuard)
   @Roles(UserRole.ADMIN, UserRole.MENTOR)
+  @ApiOperation({ summary: 'Obtenir le nombre de favoris pour un mentor' })
+  @ApiParam({ name: 'mentorId', type: String })
+  @ApiResponse({ status: 200, description: 'Statistiques des favoris mentor' })
   async getMentorFavorisStatistics(@Param('mentorId') mentorId: string) {
     return this.favoriteService.getMentorFavoriteCount(mentorId);
   }
   @Get('admin/statistics')
   @UseGuards(AuthRolesGuard)
   @Roles(UserRole.ADMIN)
+  @ApiOperation({ summary: 'Obtenir les statistiques globales des favoris' })
+  @ApiResponse({
+    status: 200,
+    description: 'Statistiques des favoris administrateur',
+  })
   async getAdminStatistics() {
     return this.favoriteService.getAdminStatistics();
   }
 
   @Get('blog/favorites/:blogId')
+  @ApiOperation({ summary: 'Obtenir le nombre de favoris pour un blog' })
+  @ApiParam({ name: 'blogId', type: String })
+  @ApiResponse({ status: 200, description: 'Nombre de favoris du blog' })
   // @UseGuards(AuthRolesGuard)
   // @Roles(UserRole.NORMAL_USER)
   async getBlogFavorites(@Param('blogId') blogId: string) {
